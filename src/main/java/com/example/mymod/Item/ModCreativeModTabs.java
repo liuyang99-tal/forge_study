9c53faf63fb4b4a4f572ccba725c3f4fbffc8578
nonepro <noneprobanru@gmail.com>
2024-04-28 22:20:09 +0800

    新增自定义block

--- src/main/java/com/example/mymod/Item/ModCreativeModTabs.java
+++ src/main/java/com/example/mymod/Item/ModCreativeModTabs.java
@@ -27,6 +27,7 @@ public class ModCreativeModTabs {
                         pOutput.accept(Items.DIAMOND);
 
                         pOutput.accept(ModBlocks.SAPPHIRE_BLOCK.get());
+                        pOutput.accept(ModBlocks.RAW_SAPPHIRE_BLOCK.get());
 
                     })
                     .build());
--- src/main/java/com/example/mymod/block/ModBlocks.java
+++ src/main/java/com/example/mymod/block/ModBlocks.java
@@ -8,6 +8,7 @@ import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -19,6 +20,9 @@ public class ModBlocks {
 
     public static final RegistryObject<Block> SAPPHIRE_BLOCK = registerBlock("sapphire_block",
             () -> new Block(BlockBehaviour.Properties.ofFullCopy(Blocks.IRON_BLOCK)));
+
+    public static final RegistryObject<Block> RAW_SAPPHIRE_BLOCK = registerBlock("raw_sapphire_block",
+            () -> new Block(BlockBehaviour.Properties.of().sound(SoundType.AMETHYST)));
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);

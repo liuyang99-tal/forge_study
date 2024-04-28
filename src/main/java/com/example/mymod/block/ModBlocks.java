55224a3d548a70b51d0ae680bc04917a304713c6
nonepro <noneprobanru@gmail.com>
2024-04-28 22:39:35 +0800

    update

--- src/main/java/com/example/mymod/block/ModBlocks.java
+++ src/main/java/com/example/mymod/block/ModBlocks.java
@@ -19,7 +19,7 @@ public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MyMod.MODID);
 
     public static final RegistryObject<Block> SAPPHIRE_BLOCK = registerBlock("sapphire_block",
-            () -> new Block(BlockBehaviour.Properties.ofFullCopy(Blocks.IRON_BLOCK)));
+            () -> new Block(BlockBehaviour.Properties.ofFullCopy(Blocks.IRON_BLOCK).sound(SoundType.AMETHYST)));
 
     public static final RegistryObject<Block> RAW_SAPPHIRE_BLOCK = registerBlock("raw_sapphire_block",
             () -> new Block(BlockBehaviour.Properties.of().sound(SoundType.AMETHYST)));

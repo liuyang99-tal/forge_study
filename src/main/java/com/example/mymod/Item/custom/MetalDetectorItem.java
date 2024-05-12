f5f0be281e6c29e857e76d5a0145f885e9063be1
nonepro <noneprobanru@gmail.com>
2024-05-12 22:03:07 +0800

    add custom tooltip

--- src/main/java/com/example/mymod/Item/custom/MetalDetectorItem.java
+++ src/main/java/com/example/mymod/Item/custom/MetalDetectorItem.java
@@ -1,12 +1,17 @@
 package com.example.mymod.Item.custom;
 
+import java.util.List;
+
 import net.minecraft.client.resources.language.I18n;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
@@ -44,6 +49,14 @@ public class MetalDetectorItem extends Item {
 
         return InteractionResult.SUCCESS;
 
+    }
+
+    @SuppressWarnings("null")
+    @Override
+    public void appendHoverText(ItemStack pStack, Level pLevel, List<Component> pToolTipComponents,
+            TooltipFlag pIsAdvanced) {
+        pToolTipComponents.add(Component.translatable("tooltip.mymod.metal_detector.tooltip"));
+        super.appendHoverText(pStack, pLevel, pToolTipComponents, pIsAdvanced);
     }
 
     private void outputValuableCoordinates(BlockPos blockPos, Player player, Block block) {
--- src/main/java/com/example/mymod/block/custom/SoundBlock.java
+++ src/main/java/com/example/mymod/block/custom/SoundBlock.java
@@ -1,11 +1,17 @@
 package com.example.mymod.block.custom;
 
+import java.util.List;
+
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
@@ -24,6 +30,14 @@ public class SoundBlock extends Block {
 
         pLevel.playSound(pPlayer, pPos, SoundEvents.NOTE_BLOCK_DIDGERIDOO.get(), SoundSource.BLOCKS, 1f, 1f);
         return InteractionResult.SUCCESS;
+    }
+
+    @SuppressWarnings("null")
+    @Override
+    public void appendHoverText(ItemStack pStack, BlockGetter pLevel, List<Component> pToolTip,
+            TooltipFlag pFlag) {
+        pToolTip.add(Component.literal("Makes sweet sounds when right-clicked!"));
+        super.appendHoverText(pStack, pLevel, pToolTip, pFlag);
     }
 
 }

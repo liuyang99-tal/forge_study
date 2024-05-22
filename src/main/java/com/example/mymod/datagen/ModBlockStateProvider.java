b6435a06135ff40f5c402ec8fef85d5d6f25aef0
nonepro <noneprobanru@gmail.com>
2024-05-22 08:22:29 +0800

    none block part 2

--- src/main/java/com/example/mymod/datagen/ModBlockStateProvider.java
+++ src/main/java/com/example/mymod/datagen/ModBlockStateProvider.java
@@ -22,42 +22,45 @@ import net.minecraftforge.registries.RegistryObject;
 
 public class ModBlockStateProvider extends BlockStateProvider {
 
-    public ModBlockStateProvider(PackOutput output, ExistingFileHelper exFileHelper) {
-        super(output, MyMod.MODID, exFileHelper);
-    }
+        public ModBlockStateProvider(PackOutput output, ExistingFileHelper exFileHelper) {
+                super(output, MyMod.MODID, exFileHelper);
+        }
 
-    @Override
-    protected void registerStatesAndModels() {
-        blockWithItem(ModBlocks.SAPPHIRE_BLOCK);
-        blockWithItem(ModBlocks.RAW_SAPPHIRE_BLOCK);
+        @Override
+        protected void registerStatesAndModels() {
+                blockWithItem(ModBlocks.SAPPHIRE_BLOCK);
+                blockWithItem(ModBlocks.RAW_SAPPHIRE_BLOCK);
 
-        blockWithItem(ModBlocks.SAPPHIRE_ORE);
-        blockWithItem(ModBlocks.DEEPSLATE_SAPPHIRE_ORE);
-        blockWithItem(ModBlocks.END_STONE_SAPPHIRE_ORE);
-        blockWithItem(ModBlocks.NETHER_SAPPHIRE_ORE);
+                blockWithItem(ModBlocks.SAPPHIRE_ORE);
+                blockWithItem(ModBlocks.DEEPSLATE_SAPPHIRE_ORE);
+                blockWithItem(ModBlocks.END_STONE_SAPPHIRE_ORE);
+                blockWithItem(ModBlocks.NETHER_SAPPHIRE_ORE);
 
-        blockWithItem(ModBlocks.SOUND_BLOCK);
-        stairsBlock(((StairBlock) ModBlocks.SAPPHIRE_STAIRS.get()), blockTexture(ModBlocks.SAPPHIRE_BLOCK.get()));
-        slabBlock(((SlabBlock) ModBlocks.SAPPHIRE_STAIRS.get()), blockTexture(ModBlocks.SAPPHIRE_BLOCK.get()),
-                blockTexture(ModBlocks.SAPPHIRE_BLOCK.get()));
+                blockWithItem(ModBlocks.SOUND_BLOCK);
+                stairsBlock(((StairBlock) ModBlocks.SAPPHIRE_STAIRS.get()),
+                                blockTexture(ModBlocks.SAPPHIRE_BLOCK.get()));
+                slabBlock(((SlabBlock) ModBlocks.SAPPHIRE_SLAB.get()),
+                                blockTexture(ModBlocks.SAPPHIRE_BLOCK.get()),
+                                blockTexture(ModBlocks.SAPPHIRE_BLOCK.get()));
 
-        buttonBlock(((ButtonBlock) ModBlocks.SAPPHIRE_BUTTON.get()), blockTexture(ModBlocks.SAPPHIRE_BLOCK.get()));
-        pressurePlateBlock(((PressurePlateBlock) ModBlocks.SAPPHIRE_PRESSURE_PLATE.get()),
-                blockTexture(ModBlocks.SAPPHIRE_BLOCK.get()));
+                buttonBlock(((ButtonBlock) ModBlocks.SAPPHIRE_BUTTON.get()),
+                                blockTexture(ModBlocks.SAPPHIRE_BLOCK.get()));
+                pressurePlateBlock(((PressurePlateBlock) ModBlocks.SAPPHIRE_PRESSURE_PLATE.get()),
+                                blockTexture(ModBlocks.SAPPHIRE_BLOCK.get()));
 
-        fenceBlock(((FenceBlock) ModBlocks.SAPPHIRE_FENCE.get()), blockTexture(ModBlocks.SAPPHIRE_BLOCK.get()));
-        fenceGateBlock(((FenceGateBlock) ModBlocks.SAPPHIRE_FENCE_GATE.get()),
-                blockTexture(ModBlocks.SAPPHIRE_BLOCK.get()));
-        wallBlock(((WallBlock) ModBlocks.SAPPHIRE_WALL.get()), blockTexture(ModBlocks.SAPPHIRE_BLOCK.get()));
+                fenceBlock(((FenceBlock) ModBlocks.SAPPHIRE_FENCE.get()), blockTexture(ModBlocks.SAPPHIRE_BLOCK.get()));
+                fenceGateBlock(((FenceGateBlock) ModBlocks.SAPPHIRE_FENCE_GATE.get()),
+                                blockTexture(ModBlocks.SAPPHIRE_BLOCK.get()));
+                wallBlock(((WallBlock) ModBlocks.SAPPHIRE_WALL.get()), blockTexture(ModBlocks.SAPPHIRE_BLOCK.get()));
 
-        doorBlockWithRenderType(((DoorBlock) ModBlocks.SAPPHIRE_DOOR.get()),
-                modLoc("block/sapphire_door_bottom"), modLoc("block/sapphire_door_top"), "cutout");
-        trapdoorBlockWithRenderType(((TrapDoorBlock) ModBlocks.SAPPHIRE_TRAPDOOR.get()),
-                modLoc("block/sapphire_trapdoor"), true, "cutout");
-    }
+                doorBlockWithRenderType(((DoorBlock) ModBlocks.SAPPHIRE_DOOR.get()),
+                                modLoc("block/sapphire_door_bottom"), modLoc("block/sapphire_door_top"), "cutout");
+                trapdoorBlockWithRenderType(((TrapDoorBlock) ModBlocks.SAPPHIRE_TRAPDOOR.get()),
+                                modLoc("block/sapphire_trapdoor"), true, "cutout");
+        }
 
-    private void blockWithItem(RegistryObject<Block> blockRegistryObject) {
-        simpleBlockWithItem(blockRegistryObject.get(), cubeAll(blockRegistryObject.get()));
-    }
+        private void blockWithItem(RegistryObject<Block> blockRegistryObject) {
+                simpleBlockWithItem(blockRegistryObject.get(), cubeAll(blockRegistryObject.get()));
+        }
 
 }
--- src/main/java/com/example/mymod/datagen/ModItemModelProvider.java
+++ src/main/java/com/example/mymod/datagen/ModItemModelProvider.java
@@ -32,6 +32,15 @@ public class ModItemModelProvider extends ItemModelProvider {
 
         simpleBlockItem(ModBlocks.SAPPHIRE_DOOR);
         fenceItem(ModBlocks.SAPPHIRE_FENCE, ModBlocks.SAPPHIRE_BLOCK);
+        buttonItem(ModBlocks.SAPPHIRE_BUTTON, ModBlocks.SAPPHIRE_BLOCK);
+        wallItem(ModBlocks.SAPPHIRE_WALL, ModBlocks.SAPPHIRE_BLOCK);
+
+        evenSimplerBlockItem(ModBlocks.SAPPHIRE_STAIRS);
+        evenSimplerBlockItem(ModBlocks.SAPPHIRE_SLAB);
+        evenSimplerBlockItem(ModBlocks.SAPPHIRE_PRESSURE_PLATE);
+        evenSimplerBlockItem(ModBlocks.SAPPHIRE_FENCE_GATE);
+
+        trapdoorItem(ModBlocks.SAPPHIRE_TRAPDOOR);
 
     }
 
@@ -42,9 +51,31 @@ public class ModItemModelProvider extends ItemModelProvider {
 
     }
 
+    private void evenSimplerBlockItem(RegistryObject<Block> block) {
+        this.withExistingParent(MyMod.MODID + ":" + ForgeRegistries.BLOCKS.getKey(block.get()).getPath(),
+                modLoc("block/" + ForgeRegistries.BLOCKS.getKey(block.get()).getPath()));
+    }
+
+    private void trapdoorItem(RegistryObject<Block> block) {
+        this.withExistingParent(ForgeRegistries.BLOCKS.getKey(block.get()).getPath(),
+                modLoc("block/" + ForgeRegistries.BLOCKS.getKey(block.get()).getPath() + "_bottom"));
+    }
+
     private void fenceItem(RegistryObject<Block> block, RegistryObject<Block> baseBlock) {
         this.withExistingParent(ForgeRegistries.BLOCKS.getKey(block.get()).getPath(), mcLoc("block/fence_inventory"))
                 .texture("texture", new ResourceLocation(MyMod.MODID,
+                        "block/" + ForgeRegistries.BLOCKS.getKey(baseBlock.get()).getPath()));
+    }
+
+    private void buttonItem(RegistryObject<Block> block, RegistryObject<Block> baseBlock) {
+        this.withExistingParent(ForgeRegistries.BLOCKS.getKey(block.get()).getPath(), mcLoc("block/button_inventory"))
+                .texture("texture", new ResourceLocation(MyMod.MODID,
+                        "block/" + ForgeRegistries.BLOCKS.getKey(baseBlock.get()).getPath()));
+    }
+
+    private void wallItem(RegistryObject<Block> block, RegistryObject<Block> baseBlock) {
+        this.withExistingParent(ForgeRegistries.BLOCKS.getKey(block.get()).getPath(), mcLoc("block/wall_inventory"))
+                .texture("wall", new ResourceLocation(MyMod.MODID,
                         "block/" + ForgeRegistries.BLOCKS.getKey(baseBlock.get()).getPath()));
     }
 

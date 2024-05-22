37c863791e981e22dbfe2226479ca048aeb78167
nonepro <noneprobanru@gmail.com>
2024-05-23 07:28:21 +0800

    2d texture with 3d model

--- src/main/java/com/example/mymod/Item/ModItems.java
+++ src/main/java/com/example/mymod/Item/ModItems.java
@@ -11,23 +11,26 @@ import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
 public class ModItems {
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MyMod.MODID);
+        public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MyMod.MODID);
 
-    public static final RegistryObject<Item> SAPPHIRE = ITEMS.register("sapphire",
-            () -> new Item(new Item.Properties()));
-    public static final RegistryObject<Item> RAW_SAPPHIRE = ITEMS.register("raw_sapphire",
-            () -> new Item(new Item.Properties()));
+        public static final RegistryObject<Item> SAPPHIRE = ITEMS.register("sapphire",
+                        () -> new Item(new Item.Properties()));
+        public static final RegistryObject<Item> RAW_SAPPHIRE = ITEMS.register("raw_sapphire",
+                        () -> new Item(new Item.Properties()));
 
-    public static final RegistryObject<Item> METAL_DETECTOR = ITEMS.register("metal_detector",
-            () -> new MetalDetectorItem(new Item.Properties().durability(100)));
+        public static final RegistryObject<Item> METAL_DETECTOR = ITEMS.register("metal_detector",
+                        () -> new MetalDetectorItem(new Item.Properties().durability(100)));
 
-    public static final RegistryObject<Item> STRAWBERRY = ITEMS.register("strawberry",
-            () -> new Item(new Item.Properties().food(ModFoods.STRAWBERRY)));
+        public static final RegistryObject<Item> STRAWBERRY = ITEMS.register("strawberry",
+                        () -> new Item(new Item.Properties().food(ModFoods.STRAWBERRY)));
 
-    public static final RegistryObject<Item> PINE_CONE = ITEMS.register("pine_cone",
-            () -> new FuelItem(new Item.Properties(), 400));
+        public static final RegistryObject<Item> SAPPHIRE_STAFF = ITEMS.register("sapphire_staff",
+                        () -> new Item(new Item.Properties().stacksTo(1)));
 
-    public static void register(IEventBus eventBus) {
-        ITEMS.register(eventBus);
-    }
+        public static final RegistryObject<Item> PINE_CONE = ITEMS.register("pine_cone",
+                        () -> new FuelItem(new Item.Properties(), 400));
+
+        public static void register(IEventBus eventBus) {
+                ITEMS.register(eventBus);
+        }
 }
